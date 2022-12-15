7a6fbcadb69d73d46821bb0a7e15d0aa085dff85
rafaelyeghoyan <rvvebcss@gmail.com>
2022-12-15 20:30:38 +0400

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
 })
 export class AppComponent {
   title = 'MoGo';
+  form = this.fb.group({
+    email:['',[Validators.pattern(/^[a-z,0-9,\.]+@[a-z]+\.+[a-z]{2,4}$/),Validators.required]]
+  })
+  constructor(private fb:FormBuilder,private el:ElementRef){
+  }
+  resuestSubscribe(){
+    alert("Your request has been sent successfully !!!")
+  }
+  dropdownOpen(){
+    this.el.nativeElement.querySelector('.dropdown_menu').style.display = "block"
+  }
+  dropdownClose(){
+    this.el.nativeElement.querySelector('.dropdown_menu').style.display = "none"
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { AppComponent } from './app.component';
+import { AlertModule, FormModule } from '@coreui/angular';
+import {CarouselModule} from "@coreui/angular";
+import { ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AccordionModule.forRoot(),
+    AlertModule,
+    CarouselModule,
+    FormModule,
+    ReactiveFormsModule,
+    BsDropdownModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
